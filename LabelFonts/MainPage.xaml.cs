3c52b1d7af9ded1630a62d4a9fe96a8bf39ff13b
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-02-27 21:16:36 -0400

    fonts random

---
+++ LabelFonts/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LabelFonts
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] font = {"OpenSans-Bold.ttf#OpenSans-Bold", "Poppins-Black.ttf#Poppins-Black", "Raleway-Black.ttf#Raleway-Black"};
+        Random random = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void MudarFonte(object sender, EventArgs args)
+        {
+
+            label_01.FontFamily = font[random.Next(0,(font.Length - 1))];
+            label_02.FontFamily = font[random.Next(0, (font.Length - 1))];
+            label_03.FontFamily = font[random.Next(0, (font.Length - 1))];
+        }
+
+    }
+}
